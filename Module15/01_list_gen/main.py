2ccd25e17ad7e4d95e2523efa75845c2c8ad16a8
kostyan513 <kostyan513@mail.ru>
2023-04-09 21:26:41 +0300

    14.3 done: odd numbers

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+numbers = []
+number = int(input('Введите число: '))
+for i in range(1, number + 1, 2):
+    numbers.append(i)
+print('Список из нечетных чисел от одного до N:', numbers)
