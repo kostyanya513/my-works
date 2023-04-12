7265f1fe1e7c61e79cadf70664274b1e1d0bbca9
kostyan513 <kostyan513@mail.ru>
2023-04-12 21:25:07 +0300

    14.3 done: tournament

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+names = []
+first_day = []
+for i in range(8):
+    name = input('Введите имя участника: ')
+    names.append(name)
+
+for index in range(8):
+    if index % 2 == 0:
+        first_day.append(names[index])
+
+print('Первый день:', first_day)
