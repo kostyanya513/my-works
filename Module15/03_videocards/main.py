ba65eb2164c723d3b6791c311e4d0bdac940b445
kostyan513 <kostyan513@mail.ru>
2023-04-13 08:36:01 +0300

    15.3 done: video cards

--- Module15/03_videocards/main.py
+++ Module15/03_videocards/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+quantity = int(input('Количество видеокарт: '))
+list_videocard = []
+new_list = []
+maximum = 0
 
+for i in range(quantity):
+    print(i + 1, 'Видеокарта:', end = '')
+    videocard = int(input())
+    list_videocard.append(videocard)
 
+for index in list_videocard:
+    if index >= maximum:
+        maximum = index
+
+for index in list_videocard:
+    if index != maximum:
+        new_list.append(index)
+
+print('Старый список видеокарт:', list_videocard)
+print('Новый список видеокарт:', new_list)
