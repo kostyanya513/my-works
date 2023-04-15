89a73063dc04805e9de4fa0d6ab7953baba88704
kostyan513 <kostyan513@mail.ru>
2023-04-15 19:27:31 +0300

    15.6 done: running numbers

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+shift = int(input('Сдвиг: '))
+list_elements = []
+count = 1
+number_elements = int(input('Количество элементов: '))
+
+for index in range(number_elements):
+    N = int(input('Число: '))
+    list_elements.append(N)
+print('Изначальный список:', list_elements)
+
+for _ in range(shift):
+    shifted_list = []
+    new_element = list_elements[-1]
+    shifted_list.append(new_element)
+    for index in list_elements:
+        count += 1
+        shifted_list.append(index)
+        if count == number_elements:
+            break
+    count = 1
+    list_elements = shifted_list
+
+print('Сдвинутый список:', shifted_list)
