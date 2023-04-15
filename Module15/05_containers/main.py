da4d0727e2ed18c1291849cde8afb4c32ea25f67
kostyan513 <kostyan513@mail.ru>
2023-04-15 18:30:39 +0300

    15.5 done: containers

--- Module15/05_containers/main.py
+++ Module15/05_containers/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+list_containers = []
+count = 1
+quantity = int(input('Количество контейнеров: '))
+for index in range(quantity):
+    weight = int(input('Введите вес контейнера: '))
+    if weight >= 200:
+        print('Вес не должен превышать 200')
+    else:
+        list_containers.append(weight)
+
+new_container = int(input('Введите вес нового контейнера: '))
+for index in list_containers:
+    if new_container <= index:
+        count += 1
+print('Номер, который получит новый контейнер:', count)
