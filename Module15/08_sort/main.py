4246a018fc6002f2c2c111a1f481b4f6def99f79
kostyan513 <kostyan513@mail.ru>
2023-04-16 17:31:39 +0300

    15.8 done: sorting

--- Module15/08_sort/main.py
+++ Module15/08_sort/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+count = 0
+print('Изначальный список: ', end = '')
+nums = [1, 15, -3, 0, 10]
+print(nums)
+for index in range(len(nums)):
+    for comparison in range(index, len(nums)):
+        if nums[comparison] < nums[index]:
+            nums[comparison], nums[index] = nums[index], nums[comparison]
+
+print('Отсортированный список:', nums)
