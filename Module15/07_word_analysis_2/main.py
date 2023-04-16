0201415bcafaeb365f02573a89c9294b451eebc8
kostyan513 <kostyan513@mail.ru>
2023-04-16 16:26:17 +0300

    15.7 done: word analysis

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+list_words = []
+word = input('Введите слово: ')
+list_words = list(word)
+count = len(word)
+
+for index in range(count // 2):
+    if list_words[- index] == list_words[index - 1]:
+        letter = True
+    else:
+        letter = False
+        print('Слово не является палидромом')
+        break
+if letter:
+    print('Слово является палиндромом')
