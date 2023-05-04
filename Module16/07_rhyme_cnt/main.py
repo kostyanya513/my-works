cb4899a38ea75deae5215407a1723d93304c33fb
kostyan513 <kostyan513@mail.ru>
2023-05-04 10:30:29 +0300

    16.7 done: rhyme cnt

--- Module16/07_rhyme_cnt/main.py
+++ Module16/07_rhyme_cnt/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+n_man = 0
+mans = int(input('Количество человек: '))
+number = int(input('Какое число в считалке? '))
+print('Значит выбывает каждый', number, end= '')
+print('-й человек')
+members = list(range(1, mans + 1))
+
+while len(members) != 1:
+    print('\nТекущий круг людей:', members)
+    n_man = n_man + number - 1 - len(members)
+    while n_man >= len(members):
+        n_man -= len(members)
+    print('Начало с номера', members[n_man])
+
+    print('Выбывает человек под номером', members[n_man])
+    members.remove(members[n_man])
+
+print('\nОстался человек полд номером', members[n_man])
