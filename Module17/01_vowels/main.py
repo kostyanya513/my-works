3673328e848faa49d7fd0087fc258457d46dbd79
kostyan513 <kostyan513@mail.ru>
2023-05-11 14:49:54 +0300

    17.1 done: vowels

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def checking_list(i):
+    new_list = [v_l for v_l in vowel_letters if v_l == i]
+    return new_list
+
+vowel_letters = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+list = list(input('Введите текст: '))
+new = []
+for i_list in list:
+    new_list = checking_list(i_list)
+    new.extend(new_list)
+
+print('Список гласных букв:', new)
+print('Длина списка:', )
