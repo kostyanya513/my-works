7ceef6cef0522c8643d4cc96e282d54262bd5120
kostyan513 <kostyan513@mail.ru>
2023-05-18 18:59:37 +0300

    17.5 done: reversal

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def schet(list):
+    for i_index in list:
+        if i_index == 'h':
+            break
+    return list.index(i_index)
+
+list = input('Введиет строку: ')
+list_conversely = list[::-1]
+print(list[len(list) - schet(list_conversely) - 2:schet(list):-1])
