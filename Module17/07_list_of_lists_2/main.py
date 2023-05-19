f1fb88cc2cbd3284c963571e188c2ef3c873b08a
kostyan513 <kostyan513@mail.ru>
2023-05-19 13:30:52 +0300

    17.7 done: list of lists

--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -1,4 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+new = [element for index in nice_list for i_element in index for element in i_element]
+print('Ответ:', new)
