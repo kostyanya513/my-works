188b6f679e46cb9074070c3a4d3542a144e7b9c6
kostyan513 <kostyan513@mail.ru>
2023-05-19 21:07:32 +0300

    17.8 done: caesar cipher

--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+alphavit = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+     'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+     'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+word = list(input('Введите сообщение: '))
+shift = int(input('Введите сдвиг: '))
+new = [(alphavit[(alphavit.index(variable) + shift) % 33] if variable in alphavit else ' ')
+       for variable in word]
+print('Зашифрованное слово:',new)
