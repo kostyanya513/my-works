63fcc5867ac147e27561c2bc6d08d6aec338d873
kostyan513 <kostyan513@mail.ru>
2023-05-28 21:23:03 +0300

    18.7 done: ip adress

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+#while True:
+ip = input('Введите IP: ')
+iip = ip.split('.')
+if ip.find(',') > 0:
+    print('Адрес - это четыре числа, разделенные точками.')
+elif True:
+    for index in range(len(iip)):
+        if not iip[index].isdigit():
+            print(iip[index], '- это не целое число')
+            break
+        elif int(iip[index]) > 255:
+            print(iip[index], 'превышает 255.')
+            break
+        else:
+            print('IP-корректен')
