af5f000a58061d6180eb869c6dad66ff82200d7f
kostyan513 <kostyan513@mail.ru>
2023-06-14 23:16:43 +0300

    19.4 done: frequency hust 2

--- Module19/04_frequency_hist_2/main.py
+++ Module19/04_frequency_hist_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def all_letters(list_text):
+  all_letter = dict()
+  for i in list_text:
+    if i in all_letter:
+      all_letter[i] += 1
+    else:
+      all_letter[i] = 1
+  return all_letter
+
+text = input('Введите текст: ').lower()
+list = all_letters(text)
+print('Оригинальный словарь частот:')
+for index, znach in sorted(list.items()):
+  print(index, ':', znach)
+schetchik = dict()
+
+print()
+print('Инвертированный словарь частот:')
+for index in list:
+  if not list[index] in schetchik:
+    schetchik[list[index]] = []
+for index in schetchik:
+  for i in list:
+    if index == list[i]:
+      schetchik[index].append(i)
+  print(index, ':', schetchik[index])
