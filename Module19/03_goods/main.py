8c0c58976aef62fcee2f347161df03d6e69931da
kostyan513 <kostyan513@mail.ru>
2023-06-14 23:02:24 +0300

    19.3 done: goods

--- Module19/03_goods/main.py
+++ Module19/03_goods/main.py
@@ -1,10 +1,32 @@
+import math
+
+def all_costs(product):
+    cost_goods = dict()
+    for y in product:
+        cost_goods['prod'] = product[y]
+        break
+    cost_goods['summa_za_vd'] = math.prod(dict.values(product))
+    return cost_goods
+
+def funkciya(string):
+    tovar = dict()
+    product_in_stock = []
+    products_stock = []
+    for i in string:
+        all_costs(i)
+        product_in_stock.append(all_costs(i))
+    for d in product_in_stock:
+        for k in d.keys():
+            tovar[k] = tovar.get(k, 0) + d[k]
+    products_stock.append(tovar)
+    return products_stock
+
 goods = {
     'Лампа': '12345',
     'Стол': '23456',
     'Диван': '34567',
     'Стул': '45678',
 }
-
 store = {
     '12345': [
         {'quantity': 27, 'price': 42},
@@ -24,4 +46,10 @@ store = {
     ],
 }
 
-# TODO здесь писать код
+for index in store:
+  store[index] = funkciya(store[index])
+
+for index in goods:
+  p = store[goods[index]][0]['prod']
+  s = store[goods[index]][0]['summa_za_vd']
+  print(f'{index} - {p} штук, стоимость {s} рублей')
