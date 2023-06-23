02a34172ae5107e7ef561e6aa840975f8afc5adb
kostyan513 <kostyan513@mail.ru>
2023-06-23 18:52:29 +0300

    19.6 done: pizza

--- Module19/06_pizza/main.py
+++ Module19/06_pizza/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+number = int(input('Введите количество заказов: '))
+all_zakaz = dict()
+for index in range(number):
+  zakaz = input(f'{index + 1}-й заказ: ').split()
+  if not zakaz[0] in all_zakaz:
+    all_zakaz[zakaz[0]] = {zakaz[1]:int(zakaz[2])
+    }
+  else:
+    if not zakaz[1] in all_zakaz[zakaz[0]]:
+      all_zakaz[zakaz[0]][zakaz[1]] = int(zakaz[2])
+    else:
+      all_zakaz[zakaz[0]][zakaz[1]] += int(zakaz[2])
+for index in all_zakaz:
+  print(f'{index}:')
+  for pizza in all_zakaz[index]:
+    print(f'{pizza}: {all_zakaz[index][pizza]}')
