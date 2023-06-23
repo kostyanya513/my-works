4eab48f590a02d5e827416556f6a5e5afcd32dc1
kostyan513 <kostyan513@mail.ru>
2023-06-23 17:00:27 +0300

    19.8 done: palindrome again

--- Module19/08_palindrome_again/main.py
+++ Module19/08_palindrome_again/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+summ = 0
+letters = {}
+for letter in text:
+  if letter in letters:
+    letters[letter] += 1
+  else:
+    letters[letter] = 1
+for letter in letters:
+  if letters[letter] % 2 > 0:
+    summ += 1
+
+if summ > 1:
+  print('Нельзя сделать палиндромом')
+else:
+  print('Можно сделать палиндромом')
