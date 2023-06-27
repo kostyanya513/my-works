a46359b2bf6112fd3b9db0f7875971d358c7080d
kostyan513 <kostyan513@mail.ru>
2023-06-27 16:25:21 +0300

    19.5 fixed: synonym dict

--- Module19/05_synonym_dict/main.py
+++ Module19/05_synonym_dict/main.py
@@ -1,16 +1,17 @@
 number = int(input('Введите количество пар слов: '))
-list_pair = [input(f'{index + 1} парa: '.lower()).split(' - ')
-             for index in range(number)]
-summ = 0
-word = input('Введите слово: ')
-for index in list_pair:
-  if word == index[0]:
-    print('Синоним:', index[1])
-    break
-  elif word == index[1]:
-    print('Синоним:', index[0])
-    break
-  else:
-    summ += 1
-if summ == number:
-  print('Такого слова нет в слова в словаре.')
+list = dict()
+list1 = dict()
+for index in range(number):
+  print(f'{index +1} пара:', end=' ')
+  list_pair = input().lower().split(' - ')
+  list[list_pair[0]] = list_pair[1]
+  list1[list_pair[1]] = list_pair[0]
+print(list)
+print(list1)
+word = input('Введите слово: ').lower()
+if word in list:
+  print('Синоним:', list[word])
+elif word in list1:
+  print('Синоним:', list1[word])
+else:
+  print('нет')
