6002a4041bc93008d43b428e7c8f2bc593052e69
kostyan513 <kostyan513@mail.ru>
2023-06-27 16:14:01 +0300

    19.1 fixed: songs 2

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -1,18 +1,22 @@
 violator_songs = {
-    'World in My Eyes': 4.86,
-    'Sweetest Perfection': 4.43,
-    'Personal Jesus': 4.56,
-    'Halo': 4.9,
-    'Waiting for the Night': 6.07,
-    'Enjoy the Silence': 4.20,
-    'Policy of Truth': 4.76,
-    'Blue Dress': 4.29,
-    'Clean': 5.83
+   'World in My Eyes': 4.86,
+   'Sweetest Perfection': 4.43,
+   'Personal Jesus': 4.56,
+   'Halo': 4.9,
+   'Waiting for the Night': 6.07,
+   'Enjoy the Silence': 4.20,
+   'Policy of Truth': 4.76,
+   'Blue Dress': 4.29,
+   'Clean': 5.83
 }
 
 songs = int(input('Сколько песен выбрать? '))
-time = []
+s = 0
 for trek in range(1, songs + 1):
-    song = input('Введите название {} песни: '.format(trek))
-    time.append(violator_songs[song])
-print('Общее время звучания песен: {} минуты'.format(round(sum(time), 2)))
+  song = input('Введите название {} песни: '.format(trek))
+  if song in violator_songs:
+    s += violator_songs[song]
+  else:
+    print('Такой песни нет')
+    break
+print('Общее время звучания песен: {} минуты'.format(round(s, 2)))
