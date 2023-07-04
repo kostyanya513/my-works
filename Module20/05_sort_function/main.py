1d81a189efa56d420040c662bbf75a58f792d187
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-04 11:00:30 +0300

    done 20.5: sort function

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def tpl_sort(string):
+    for index in string:
+        if not isinstance(index, int):
+            return string
+    return tuple(sorted(string))
+
+tpl = (6, 3, -1, 8, 4, 10, -5)
+print(tpl_sort(tpl))
