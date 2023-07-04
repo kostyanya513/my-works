131b44bec27c6da5e62f4e849cfb6abb9136ba9b
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-04 11:54:53 +0300

    done 20.6: contacts 3

--- Module20/06_contacts_3/main.py
+++ Module20/06_contacts_3/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+glossary = {}
+while True:
+    action = input('Введите действие:'
+                   '\n1. Добавить человека'
+                   '\n2. Найти человека'
+                   '\n')
+    if action == '1':
+        name = input('Введите имя и фамилию нового контакта (через пробел): ').split(' ')
+        phon = int(input('Введите номер телефона: '))
+        glossary[(name[0], name[1])] = phon
+        print('Текущий словарь контактов:', glossary)
+    elif action == '2':
+        for man in glossary:
+            search_name = input('Введите фамилию для поиска: ')
+            if search_name in man:
+                print(man[0], man[1], glossary[man])
+    elif action == 'end':
+        print('Работа завершена!')
+        break
+    else:
+        print('Повторите ввод!')
