e0000505288afa0d38b7ed49faef6f0da20dadb9
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-04 12:08:06 +0300

    done 20.2: universal prog 2

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,15 +1,13 @@
 # TODO здесь писать код
 def is_prime(variable, val):
-    k = 0
+    ratio = 0
     for i_index in range(2, (variable // 2) + 1):
         if variable % i_index == 0:
-            k += 1
-    if k == 0:
+            ratio += 1
+    if ratio == 0:
         return val
 
 def crypto(string):
-    string = list(string)
-    s = [values for index, values in enumerate(string) if is_prime(index, values)]
-    return s
+    return [values for index, values in enumerate(list(string)) if is_prime(index, values)]
 
 print(crypto((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
--- Module20/07_my_zip/main.py
+++ Module20/07_my_zip/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+i_list = 'abcd'
+i_tuple = (10, 20, 30, 40)
+i_zip = zip(i_list, i_tuple)
+print(i_zip)
+for index in i_zip:
+    print(index)
