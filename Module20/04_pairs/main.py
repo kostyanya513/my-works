38bac800bc21fc383497dd3de2d84ccd3755b0ac
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-04 10:02:49 +0300

    done 20.4: pairs

--- Module20/04_pairs/main.py
+++ Module20/04_pairs/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+string = [random.randint(0, 100) for _ in range(10)]
+print('Оригинальный описок:', string)
+list_pair = []
+for index in range(5):
+    pair = (string[index * 2], string[index * 2 + 1])
+    list_pair.append(pair)
+print('Новый список:', list_pair)
