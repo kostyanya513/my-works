ed3be6cc538ac1e13088943d23809d81192ee974
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-04 12:54:39 +0300

    done 20.7: my zip

--- Module20/07_my_zip/main.py
+++ Module20/07_my_zip/main.py
@@ -1,6 +1,16 @@
-i_list = 'abcd'
-i_tuple = (10, 20, 30, 40)
+import random
+import string
+
+i_list = [random.choice(string.ascii_letters) for _ in range(5)]
+i_tuple = [random.randint(1, 10) for _ in range(5)]
+i_tuple_2 = (random.randint(1, 10) for _ in range(5))
+
 i_zip = zip(i_list, i_tuple)
 print(i_zip)
 for index in i_zip:
+    print(index)
+
+i_zip_2 = zip(i_list, i_tuple_2)
+print(i_zip_2)
+for index in i_zip_2:
     print(index)
