0400c7ecd64c4827c2a8cc430e0a73413f38f1be
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-08 11:49:15 +0300

    done 21.1: challenge 2

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def sequence(number):
+    print(number)
+    if number == 1:
+        return 1
+    return sequence(number - 1)
+
+
+num = int(input('Введите num: '))
+sequence(num)
