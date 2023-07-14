c250e178f12f7ec5d58fb42d465206a55dcd0a4b
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-14 16:23:03 +0300

    21.3 done: deep copy

--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -10,5 +10,27 @@ site = {
         }
     }
 }
+import copy
 
-# TODO здесь писать код
+
+def creating_website(structure_site, product):
+    if isinstance(structure_site, (int, float, bool, str)):
+        new_list = []
+        for index_struc in structure_site.split():
+            if index_struc == 'телефон' or index_struc == 'iphone':
+                index_struc = product
+            new_list.append(index_struc)
+        return ' '.join(new_list)
+    elif isinstance(structure_site, dict):
+        return {key: creating_website(value, product) for key, value in structure_site.items()}
+
+
+list_site = []
+quantity = int(input('Сколько сайтов: '))
+for website in range(quantity):
+    name_website = input('Введите название продукта для нового сайта:  ')
+    print('Сайт для {}:'.format(name_website))
+    next_site = copy.deepcopy(site)
+    list_site.append(creating_website(next_site, name_website))
+    for index in range(len(list_site)):
+        print('sate =', list_site[index])
