c09cf0bf55af11a8ab98f0668c20dcc788cd4b99
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-14 17:24:31 +0300

    21.4 done: advanced sum

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def sum(terms, summ=0):
+    if isinstance(terms, (int, float)):
+        summ += terms
+    elif isinstance(terms, list):
+        for next_term in terms:
+            summ += sum(next_term)
+        else:
+            sum(next_term)
+
+    return summ
+
+
+list_terms = [1, 2, 3, 4, 5]
+print(sum(list_terms))
