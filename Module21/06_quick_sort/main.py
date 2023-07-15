88f4f9286e42b3aaac872fc7d4b2e2bac582c7ce
Kot <kostyan513@mail.ru>
2023-07-16 00:14:58 +0300

    21.6 done: quick sort

--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,18 +1,34 @@
-def split_list(numbers):
-    if len(numbers) <= 1:
-        return numbers
-    support = numbers[-1]
-    left = []
-    middle = []
-    right = []
-    for a in numbers:
-        if a < support:
-            left.append(a)
-        elif a == support:
-            middle.append(a)
+# TODO здесь писать код
+def split_three_list(structure, num,
+                     struc=[], rezult_1=[], rezult_2=[], rezult_3=[]
+                     ):
+    for i in structure:
+        if i < num:
+            rezult_1.append(i)
+        elif i == num:
+            rezult_2.append(i)
         else:
-            right.append(a)
-    return split_list(left) + middle + split_list(right)
+            rezult_3.append(i)
+    struc.append(rezult_1)
+    struc.append(rezult_2)
+    struc.append(rezult_3)
+    print(struc)
+    return struc
+
+
+def sorting_list(numbers, v = [], t=None):
+    number = numbers[-1]
+    z = split_three_list(numbers, number)
+    for i in z:
+        n = i[-1]
+        t = split_three_list(i, n)
+        return t
+    v.append(t)
+    return v
+#    print(t)
+#    print(number)
+#    print(split_three_list(numbers, number))
+
 
 list_number = [5, 8, 9, 4, 2, 9, 1, 8]
-print(split_list(list_number))
+print(sorting_list(list_number))
