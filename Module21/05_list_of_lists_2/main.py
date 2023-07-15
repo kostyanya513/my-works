be5071f9e37680762a0b9af13e81957d3e28f576
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-15 09:52:02 +0300

    21.5 done: list of lists 2

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+def expanded_list(structure, new_list = []):
+    if isinstance(structure, (int, float, str)):
+        new_list.append(structure)
+    elif isinstance(structure, list):
+        for element in structure:
+            rezult = expanded_list(element)
+    return new_list
+
+
+print('Ответ: ', expanded_list(nice_list))
