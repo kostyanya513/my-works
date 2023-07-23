27a0912fe006dafa882d6db726215f08a1d5a25a
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-23 14:11:50 +0300

    done 22.1: nums sum

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+nums = open('numbers.txt', 'r', encoding='utf-8')
+summ = 0
+for elem in nums:
+    if elem.join(elem.split()):
+        summ += int(elem.join(elem.split()))
+nums.close()
+
+answer = open('answer.txt', 'w')
+writing_to_file = answer.write(str(summ))
+answer.close()
