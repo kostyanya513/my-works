54995de9d23cd0bebf72ec8c9b0ed313e0e70996
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-07-23 14:41:29 +0300

    done 22.2: zen of python

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+text = open('zen.txt', 'r')
+data = text.read()
+text_conversely = []
+for string in data.split('\n'):
+    text_conversely.append(string)
+print('\n'.join(text_conversely[::-1]))
+text.close()
