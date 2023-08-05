e6f47d866d8453b1a9dea203280a2326bacbd679
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-05 18:11:02 +0300

    done 22.5: frequency analysis

--- Module22/05_frequency_analysis/main.py
+++ Module22/05_frequency_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z')
+text_file = open('text.txt', 'r')
+data = text_file.read().lower().split()
+list_data = ''.join(data)
+quantity = {}
+for letter in list_data:
+    if letter in alphabet:
+        quantity.setdefault(letter, 0)
+        quantity[letter] += 1 / len(list_data)
+sorted_quantity = {key: value for key, value in sorted(quantity.items(), key=lambda item: (-item[1], item[0]))}
+text_file.close()
+
+analysis_file = open('analysis.txt', 'w')
+for key, value in sorted_quantity.items():
+    gistograma = analysis_file.write(key + ' ' + str(round(value, 3)) + '\n')
+analysis_file.close()
