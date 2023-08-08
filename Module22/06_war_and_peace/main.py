23988af3c8636b7acb42113255427e788918dc20
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-08 21:06:53 +0300

    fixed 22.6: v i m

--- Module22/06_war_and_peace/main.py
+++ Module22/06_war_and_peace/main.py
@@ -9,6 +9,6 @@ quantity = {}
 for letter in symbols:
     quantity.setdefault(letter, 0)
     quantity[letter] += 1
-sort_voyna_i_mir = sorted(quantity.items(), key=lambda x: x[1])
-sort_voyna_i_mir_dict = dict(sort_voyna_i_mir)
-print(sort_voyna_i_mir_dict)
+sort_data = sorted(quantity.items(), key=lambda x: x[1])
+sort_data_dict = dict(sort_data)
+print(sort_data_dict)
