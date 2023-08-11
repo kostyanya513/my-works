1e6946a6f8eea2a8d7c96e5063bd4be584d63a0a
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-11 23:28:16 +0300

    done 23.5: reliable calculations

--- Module23/05_reliable_calculations/main.py
+++ Module23/05_reliable_calculations/main.py
@@ -1,9 +1,22 @@
-# TODO здесь писать код
-
-# Здесь создайте функцию get_sage_sqrt
+import math
 
 
-# Тестовые случаи
+def get_sage_sqrt(num):
+    try:
+        if isinstance(num, int):
+            return math.sqrt(num)
+        elif isinstance(num, float):
+            return math.sqrt(num)
+        elif isinstance(num, str):
+            raise SyntaxError
+        elif num < 0:
+            raise ValueError
+    except ValueError as exc:
+        return type(exc), 'Отрицательное число'
+    except SyntaxError as exc:
+        return type(exc), 'Строка'
+
+
 numbers = [16, 25, -9, 0, 4.5, "abc"]
 for number in numbers:
     result = get_sage_sqrt(number)
