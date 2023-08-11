238ac3e7260ab32ec60a2523c35e8bec8a2423e3
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-11 21:22:24 +0300

    done 23.2: lucky number

--- Module23/02_lucky_number/main.py
+++ Module23/02_lucky_number/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import random
+summ = 0
+with open('out_file.txt', 'w', encoding='utf-8') as numbers:
+    try:
+        while summ < 777:
+            nums = int(input('Введите число: '))
+            random_number = random.randint(1, 13)
+            if random_number == 1:
+                raise BaseException
+            numbers.write(str(nums) + '\n')
+            summ += nums
+        print('Вы успешно выполнили условие для выхода из цикла!')
+    except BaseException:
+        print('Вас постигла неудача!')
