2951577802504662d4a9a937e9e0b53770fb29df
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-12 10:12:21 +0300

    done 23.4: chat

--- Module23/04_chat/main.py
+++ Module23/04_chat/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def change_action(act):
+    if act == '1':
+        with open('chat_users.txt', 'r', encoding='utf-8') as messages_user:
+            print(messages_user.read())
+    elif act == '2':
+        message = input('Введите сообщение: ')
+        return chat.write(user + ': ' + message + '\n')
+
+
+while True:
+    chat = open('chat_users.txt', 'a', encoding='utf-8')
+    user = input('Имя пользователя: ')
+    action = input('Выберите одно из действий: 1. Посмотреть текст чата; 2. Отправить сообщение: ')
+    change_action(action)
+    chat.close()
