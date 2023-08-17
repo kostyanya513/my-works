83714f8525996676f12ac5865cd86ef47caef16f
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-08-17 20:48:31 +0300

    done 24.1: fight

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import random
+
+
+class Warrior:
+
+    def __init__(self, health=100, viktory=False):
+        self.health = health
+        self.viktory = viktory
+
+    def loss_health(self, user):
+        self.health -= 20
+        print('Здоровье {} игрока: {}'.format(user, self.health))
+        if self.health == 0:
+            return True
+
+    def info(self, n=0):
+        print('Здоровье {} игрока: {}'.format(n, self.health))
+
+
+warrior_first = Warrior()
+warrior_second = Warrior()
+warrior_first.info(1)
+warrior_second.info(2)
+for i in range(20):
+    number = random.randint(1, 2)
+    if number == 1:
+        if warrior_first.loss_health(number):
+            print('Победа 2 игрока')
+            break
+    else:
+        if warrior_second.loss_health(number):
+            print('Победа 1 игрока')
+            break
