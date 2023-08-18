0690411d7d1375fad611e62a10493144d5a4e0c5
Kot <kostyan513@mail.ru>
2023-08-18 22:29:23 +0300

    done

--- Module24/03_fathers_mothers_kids/main.py
+++ Module24/03_fathers_mothers_kids/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+class Parent:
+    name = 'Константин'
+    age = 38
+    children = 'Валерий'
+
+    def print_info(self):
+        print('Имя родителя: {}\nВозраст: {}\nСписок детей {}'.format(
+            self.name, self.age, self.children
+        ))
+
+    def eat_child(self):
+        if child.hungry == True:
+            child.hungry = False
+            print('\nРебенок накормлен!')
+            child.print_info_child()
+
+    def calm(self):
+        if child.calm == False:
+            child.calm = True
+            print('\nРебенок успокоился!')
+            child.print_info_child()
+
+class Children:
+
+    def __init__(self, name, age, calm, hungry):
+        self.name = name
+        self.age = age
+        self.calm = calm
+        self.hungry = hungry
+
+    def print_info_child(self):
+        print('\nИмя ребенка: {}\nВозраст ребенка: {}\nСостояние спокойствия: {}\nСостояние голода: {}'.format(
+            self.name, self.age, self.calm, self.hungry
+        ))
+
+
+parent = Parent()
+child = Children('Валерий', 15, False, True)
+parent.print_info()
+child.print_info_child()
+parent.calm()
+parent.eat_child()
+
--- Module24/05_cohabitation/main.py
+++ Module24/05_cohabitation/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+import random
+class Person:
+
+    def __init__(self, name=None, calm=50, house=True):
+        self.name = name
+        self.calm = calm
+        self.house = house
+
+    def
+
+class House:
+
+    def __init__(self, holodilnik=50, tumb=0):
+        self.holodilnik = holodilnik
+        self.tumb = tumb
