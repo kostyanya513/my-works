04ef0f7a58f8d72462b75becca3d732d265dd7cd
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-09-06 21:58:33 +0300

    done 24.7: matrix

--- Module24/07_Matrix/main.py
+++ Module24/07_Matrix/main.py
@@ -1,9 +1,51 @@
-# TODO здесь писать код
+class Matrix:
+    def __init__(self, strok, stolbec):
+        self.strok = strok
+        self.stolbec = stolbec
+        self.data = [[0 for _ in range(stolbec)] for _ in range(strok)]
 
+    def add(self, other):
+        if self.strok != other.strok or self.stolbec != other.stolbec:
+            raise ValueError('Матрицы должны быть одинаковы')
+        result = Matrix(self.strok, self.stolbec)
+        for i in range(self.strok):
+            for j in range(self.stolbec):
+                result.data[i][j] = self.data[i][j] + other.data[i][j]
+        return result
 
+    def subtract(self, other):
+        if self.strok != other.strok or self.stolbec != other.stolbec:
+            raise ValueError('Матрицы должны быть одинаковы')
+        result = Matrix(self.strok, self.stolbec)
+        for i in range(self.strok):
+            for j in range(self.stolbec):
+                result.data[i][j] = self.data[i][j] - other.data[i][j]
+        return result
 
+    def multiply(self, other):
+        if self.stolbec != other.strok:
+            raise ValueError('Количество столбцов первой матрицы должно быть равно количеству строк второй матрицы!')
+        result = Matrix(self.strok, other.stolbec)
+        for i in range(self.strok):
+            for j in range(other.stolbec):
+                for k in range(self.stolbec):
+                    result.data[i][j] = self.data[i][k] * other.data[k][j]
+        return result
 
-# Примеры работы с классом:
+    def transpose(self):
+        result = Matrix(self.stolbec, self.strok)
+        for i in range(self.strok):
+            for j in range(self.stolbec):
+                result.data[j][i] = self.data[i][j]
+        return result
+
+    def __str__(self):
+        output = ''
+        for stroc in self.data:
+            output += '\t'.join(str(element) for element in stroc)
+            output += '\n'
+        return output
+
 
 # Создание экземпляров класса Matrix
 m1 = Matrix(2, 3)
@@ -12,7 +54,7 @@ m1.data = [[1, 2, 3], [4, 5, 6]]
 m2 = Matrix(2, 3)
 m2.data = [[7, 8, 9], [10, 11, 12]]
 
-# Тестирование операций
+print('Вывод:')
 print("Матрица 1:")
 print(m1)
 
@@ -26,7 +68,9 @@ print("Вычитание матриц:")
 print(m1.subtract(m2))
 
 m3 = Matrix(3, 2)
+print("Матрица 3:")
 m3.data = [[1, 2], [3, 4], [5, 6]]
+print(m3)
 
 print("Умножение матриц:")
 print(m1.multiply(m3))
