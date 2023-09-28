4d11934163ec674fd1958f5eb1a09080adf875c4
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-09-28 12:06:02 +0300

    FIXED 24.5: cohabitation

--- Module24/05_cohabitation/main.py
+++ Module24/05_cohabitation/main.py
@@ -1,69 +1,70 @@
 import random
 
 
-class Person:
-    def __init__(self, name=None, calm=50, house=True):
-        self.numb = None
-        self.name = name
-        self.calm = calm
-        self.house = house
-
-    def print_info(self):
-        print('Имя: {}; Степень сытости: {}; Дом: {}'.format(
-            self.name, self.calm, self.house
-        ))
-
-    def eat(self, numb):
-        man.calm += numb
-        home.fridge -= numb
-
-    def work(self, numb):
-        self.numb = numb
-        man.calm -= numb
-        home.money += numb
-
-    def play(self, numb):
-        man.calm -= numb
-
-    def go(self, numb):
-        home.fridge += numb
-        home.money -= numb
-
-    def action(self, numb):
-        for day in range(365):
-            if man.calm <= 0:
-                man.house = False
-                print('{} погибли от голода'.format(self.name))
-            if man.calm < 20:
-                man.eat(numb)
-            elif home.fridge < 10:
-                man.go(numb)
-            elif home.money < 50:
-                man.work(numb)
-            elif numb == 1:
-                man.work(numb)
-            elif numb == 2:
-                man.eat(numb)
-            else:
-                man.play(numb)
-
-
 class House:
-    def __init__(self, fridge=50, money=0):
-        self.fridge = fridge
-        self.money = money
+    def __init__(self, holodilnik=50, tumb=0):
+        self.holodilnik = holodilnik
+        self.tumb = tumb
 
     def print_info_house(self):
-        print('Еды в холодильнике: {}, Денег в тумбочке: {}'.format(
-            self.fridge,
-            self.money
+        print('  : {},   : {}'.format(
+            self.holodilnik,
+            self.tumb
         ))
 
 
-man = Person('Иван и Федя')
-home = House()
+class Person:
+    def __init__(self, name, calm=50):
+        self.name = name
+        self.calm = calm
+        self.house = House()
+
+    def print_info(self):
+        print(': {};  : {}'.format(
+            self.name, self.calm
+        ))
+        self.house.print_info_house()
+
+    def eat(self, number):
+        self.calm += number
+        self.house.holodilnik -= number
+
+    def work(self, number):
+        self.calm -= number
+        self.house.tumb += number
+
+    def play(self, number):
+        self.calm -= number
+
+    def go_shop(self, number):
+        self.house.holodilnik += number
+        self.house.tumb -= number
+
+    def action(self, number):
+        if self.calm <= 0:
+            print(' !')
+            raise ValueError('')
+        if self.calm < 20:
+            self.eat(number)
+        elif self.house.holodilnik < 10:
+            self.go_shop(number)
+        elif self.house.tumb < 50:
+            self.work(number)
+        elif number == 1:
+            self.work(number)
+        elif number == 2:
+            self.eat(number)
+        else:
+            self.play(number)
+
+
+man_1 = Person('')
+man_2 = Person('')
+house = House()
 for _ in range(365):
-    number = 2 * random.randint(1, 6)
-    man.action(number)
-man.print_info()
-home.print_info_house()
+    number_1 = random.randint(1, 6)
+    number_2 = random.randint(1, 6)
+    man_1.action(number_1)
+    man_2.action(number_2)
+man_1.print_info()
+man_2.print_info()
