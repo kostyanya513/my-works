e9e34b8d5f2e0cedf74e54ed6b61802fa6346772
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-10-19 11:04:35 +0300

    FIXED 25.3: my dict

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+class Dict:
+    __slovar = {1: 'first', 2: 'second', 3: 'therd'}
+
+    def get_slovar(self):
+        return self.__slovar
+
+    def get(self, key):
+        if key in self.__slovar:
+            return self.__slovar[key]
+        else:
+            return None
+
+
+class MyDict(Dict):
+    def get(self, key):
+        if key in self.get_slovar():
+            return self.get_slovar()[key]
+        else:
+            return 0
+
+
+mydict = MyDict()
+print(mydict.get(4))
