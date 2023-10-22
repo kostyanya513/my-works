de30266576febccf324a7794118c5f8b2c6425f8
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-10-22 14:42:04 +0300

    FIXED 25.6: Abstract class

--- Module25/06_Abstract_class/main.py
+++ Module25/06_Abstract_class/main.py
@@ -1,19 +1,59 @@
-# TODO здесь писать код
+import math
+from abc import ABC, abstractmethod
 
 
+class Shape(ABC):
+    def __init__(self):
+        self.s = None
 
-# Примеры работы с классом:
+    @abstractmethod
+    def area(self):
+        pass
+
+    def print_area(self):
+        return self.s
+
+
+class Circle(Shape):
+    def __init__(self, r):
+        super().__init__()
+        self.r = r
+
+    def area(self):
+        self.s = math.pi * self.r * 2
+
+
+class Rectangle(Shape):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+
+    def area(self):
+        self.s = self.a * self.b
+
+
+class Triangle(Shape):
+    def __init__(self, a, b):
+        super().__init__()
+        self.a = a
+        self.b = b
+
+    def area(self):
+        self.s = self.a * self.b / 2
+
+
 # Создание экземпляров классов
 circle = Circle(5)
 rectangle = Rectangle(4, 6)
 triangle = Triangle(3, 8)
 
 # Вычисление площади фигур
-circle_area = circle.area()
-rectangle_area = rectangle.area()
-triangle_area = triangle.area()
+circle.area()
+rectangle.area()
+triangle.area()
 
 # Вывод результатов
-print("Площадь круга:", circle_area)
-print("Площадь прямоугольника:", rectangle_area)
-print("Площадь треугольника:", triangle_area)
+print("Площадь круга:", circle.print_area())
+print("Площадь прямоугольника:", rectangle.print_area())
+print("Площадь треугольника:", triangle.print_area())
