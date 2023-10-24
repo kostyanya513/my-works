dab456e1efd70f3a76709d1b6dca91bd2e557896
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-10-24 22:25:41 +0300

    done 25.4: RPG game

--- Module25/05_stack/main.py
+++ Module25/05_stack/main.py
@@ -13,33 +13,34 @@ class TaskManager:
     """
     Класс Менеджер задач
     Attributes:
-        d (dict): звдачи
+        task_list (dict): звдачи
     """
     def __init__(self):
-        self.d = {}
+        self.task_list = {}
 
-    def new_task(self, a, b):
+    def new_task(self, task, priority):
         """
         Метод добавления задачи в словарь задач
-        :param a: (str) задача
-        :param b: (int) приоритет
+        :param task: (str) задача
+        :param priority: (int) приоритет
         """
-        if b in self.d.keys():
-            self.d[b].append(a)
+        if priority in self.task_list.keys():
+            self.task_list[priority].append(task)
         else:
-            self.d[b] = [a]
+            self.task_list[priority] = [task]
 
     def __str__(self):
         """
         Магический метод вывода задач по приоритету: чем меньше число, тем выше задача
-        k (int): ключ
-        v (str): значение
+        key (int): ключ
+        value (str): значение
+        result (str): результат для вывода на экран
         """
-        sort = sorted(self.d.items())
-        rez = ''
-        for k, v in sort:
-            rez += '{}: '.format(k) + '; '.join(v) + '\n'
-        return 'Результат:\n{}'.format(rez)
+        sort = sorted(self.task_list.items())
+        result = ''
+        for key, value in sort:
+            result += '{}: '.format(key) + '; '.join(value) + '\n'
+        return 'Результат:\n{}'.format(result)
 
 
 manager = TaskManager()
