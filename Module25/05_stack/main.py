0243160256e2dcbfe5acc214ec13e3e7cc9e9bc8
Kot <kostyan513@mail.ru>
2023-10-24 11:44:16 +0300

    done 24.4: change magic

--- Module25/05_stack/main.py
+++ Module25/05_stack/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+class Stek:
+    def __init__(self):
+        self.m = []
+
+    def put(self, c):
+        return self.m.append(c)
+
+    def pop(self):
+        self.m.pop()
+
+
+class TaskManager:
+    def __init__(self):
+        self.d = {}
+
+    def new_task(self, a, b):
+        if b in self.d.keys():
+            self.d[b].append(a)
+        else:
+            self.d[b] = [a]
+
+    def __str__(self):
+        sort = sorted(self.d.items())
+        rez = ''
+        for k, v in sort:
+            rez += '{}: '.format(k) + '; '.join(v) + '\n'
+        return 'Результат:\n{}'.format(rez)
+
+
+manager = TaskManager()
+manager.new_task("сделать уборку", 4)
+manager.new_task("помыть посуду", 4)
+manager.new_task("отдохнуть", 1)
+manager.new_task("поесть", 2)
+manager.new_task("сдать дз", 2)
+print(manager)
