de353f1b6e619b93092f32173b90c05fe39f9a97
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-10-24 22:06:58 +0300

    done 25.6: Abstract class

--- Module25/06_Abstract_class/main.py
+++ Module25/06_Abstract_class/main.py
@@ -3,6 +3,11 @@ from abc import ABC, abstractmethod
 
 
 class Shape(ABC):
+    """
+    Абстрактный класс
+    Attrubuts:
+    s (int): площадь фигуры
+    """
     def __init__(self):
         self.s = None
 
@@ -11,35 +16,64 @@ class Shape(ABC):
         pass
 
     def print_area(self):
+        """
+        Метод вывода на экран площади фигуры
+        """
         return self.s
 
 
 class Circle(Shape):
+    """
+    Класс Круг. Родитель: Shape
+    Args:
+        r (int): радиус круга
+    """
     def __init__(self, r):
         super().__init__()
         self.r = r
 
     def area(self):
+        """
+        Переопределенный метод для расчета плащади круга
+        """
         self.s = math.pi * self.r * 2
 
 
 class Rectangle(Shape):
+    """
+    Класс Прямоугольник. Родитель: Shape
+    Args:
+        a (int): длина прямоугольника
+        b (int): ширина прямоугольника
+    """
     def __init__(self, a, b):
         super().__init__()
         self.a = a
         self.b = b
 
     def area(self):
+        """
+        Переопределенный метод для расчета плащади прямоугольника
+        """
         self.s = self.a * self.b
 
 
 class Triangle(Shape):
+    """
+    Класс Треугольник. Родитель: Shape
+    Args:
+        a (int): длина основания
+        b (int): высота треугольника
+    """
     def __init__(self, a, b):
         super().__init__()
         self.a = a
         self.b = b
 
     def area(self):
+        """
+        Переопределенный метод для расчета плащади треугольника
+        """
         self.s = self.a * self.b / 2
 
 
