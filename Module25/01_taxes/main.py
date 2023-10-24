942817a55778c619f20dec9ff399b967f610f2bf
Kot <kostyan513@mail.ru>
2023-10-24 14:38:18 +0300

    done 25.1: taxes

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,10 +1,22 @@
 class Property:
+    """
+    Базовый класс, описывающий стоимость имущества
+    Args:
+        worth (int): передается стоимость имущества
+        cost (int): передается количество денег
+    Attributes:
+        taxe (int): надог на имущество
+    """
     def __init__(self, worth, cost):
         self.worth = worth
         self.cost = cost
-        self.taxe = self.worth / 1
+        self.taxe = 0
 
-    def reshenie(self):
+    def calculation(self):
+        """
+        Метод расчета налога и количества недостающих денег
+
+        """
         if self.cost - self.taxe >= 0:
             print('Налог {}'.format(self.taxe))
         elif self.cost - self.taxe < 0:
@@ -14,27 +26,51 @@ class Property:
 
 
 class Apartment(Property):
+    """
+    Класс квартира. Родитель: Property
+    Args:
+        worth (int): передается стоимость квартиры
+        cost (int): передается количество денег
+    Attributes:
+        taxe (int): расчет надога на квартиру
+    """
     def __init__(self, worth, cost):
         super().__init__(worth, cost)
         self.taxe = self.worth / 1000
 
 
 class Car(Property):
+    """
+    Класс машина. Родитель: Property
+    Args:
+        worth (int): передается стоимость машины
+        cost (int): передается количество денег
+    Attributes:
+        taxe (int): расчет надога на машину
+    """
     def __init__(self, worth, cost):
         super().__init__(worth, cost)
         self.taxe = self.worth / 200
 
 
 class CountryHouse(Property):
+    """
+    Класс дача. Родитель: Property
+    Args:
+        worth (int): передается стоимость дачи
+        cost (int): передается количество денег
+    Attributes:
+        taxe (int): расчет надога на дачу
+    """
     def __init__(self, worth, cost):
         super().__init__(worth, cost)
         self.taxe = self.worth / 500
 
 
-costul = int(input('Стоимость имущества? '))
+property_value = int(input('Стоимость имущества? '))
 money = int(input('Сколько денег у пользователя? '))
 
-apart = Apartment(costul, money)
-car = Car(costul, money)
-countryhouse = CountryHouse(costul, money)
-apart.reshenie()
+apart = Apartment(property_value, money)
+car = Car(property_value, money)
+countryhouse = CountryHouse(property_value, money)
+apart.calculation()
