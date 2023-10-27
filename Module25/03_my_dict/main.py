e95b150cd94e74411a9224b5c28abcbc8d7e8d80
Kot <kostyan513@mail.ru>
2023-10-27 22:52:04 +0300

    fixe 25.3: my dict

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,46 +1,17 @@
-class Dict:
+class MyDict(dict):
     """
-    Базовый класс, описывающий словарь
-    __slovar: словарь
+    Класс моего словаря. Родитель: dict
     """
-    __slovar = {1: 'first', 2: 'second', 3: 'therd'}
-
-    def get_slovar(self):
+    def get(self, key, default=0):
         """
-        Геттер для получения словаря
-        :return: __slovar
-        :rtype: dict
-        """
-        return self.__slovar
-
-    def get(self, key):
-        """
-        Геттер для получения значения словаря
-        :param key: ключ
-        :return: значение или None
-        """
-        if key in self.__slovar:
-            return self.__slovar[key]
-        else:
-            return None
-
-
-class MyDict(Dict):
-    """
-    Класс моего словаря. Родитель: Dict
-    __slovar: словарь
-    """
-    def get(self, key):
-        """
-        Геттер для получения значения словаря
+        Переопределение метода get для словаря
+        :param default: 0
         :param key: ключ
         :return: значение или 0
         """
-        if key in self.get_slovar():
-            return self.get_slovar()[key]
-        else:
-            return 0
+        return super().get(key, default)
 
 
 mydict = MyDict()
-print(mydict.get(4))
+mydict['a'] = 1
+print(mydict.get('b'))
