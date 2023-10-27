a3888432135297728becce517f6a9a91b4c68703
Kot <kostyan513@mail.ru>
2023-10-27 22:32:56 +0300

    done 25.5: stack

--- Module25/05_stack/main.py
+++ Module25/05_stack/main.py
@@ -1,64 +1,81 @@
-from queue import LifoQueue
-
-
-class Stek:
-    stack = LifoQueue()
+class Stack:
+    """
+    Класс Stack
+    Atributes:
+    dict_task (dict) список задач
+    """
 
     def __init__(self):
-        self.m = []
+        self.dict_task = []
 
-    def put(self, c):
-        self.stack.put(c)
+    def __str__(self):
+        return str('; '.join(self.dict_task))
 
-    def get(self):
-        item = self.stack.get()
+    def put(self, element):
+        """
+        Метод добавления задачи в стек
+        """
+        self.dict_task.append(element)
+
+    def pop(self):
+        """
+        Метод удаления задачи из стека
+        """
+        if len(self.dict_task) == 0:
+            return None
+        return self.dict_task.pop()
 
 
 class TaskManager:
     """
     Класс Менеджер задач
     Attributes:
-        task_list (dict): звдачи
+        task_list (dict): задачи
     """
 
     def __init__(self):
-
         self.task_list = {}
 
     def new_task(self, task, priority):
         """
-        Метод добавления задачи в словарь задач
-        :param task: (str) задача
+        Метод добавления задачи в стек, если уже есть задачи с таким приоритетом.
+        Или добавляется стек и одна задача в него.
+        """
+        if priority in self.task_list.keys():
+            new_task = Stack()
+            while len(str(self.task_list[priority])) != 0:
+                value = self.task_list[priority].pop()
+                if value != task:
+                    new_task.put(value)
+            new_task.put(task)
+            self.task_list[priority] = new_task
+        else:
+            self.task_list[priority] = Stack()
+            self.task_list[priority].put(task)
+
+    def delete_task(self, priority):
+        """
+        Метод удаления задачи из стека, если такая имеется
         :param priority: (int) приоритет
         """
         if priority in self.task_list.keys():
-            self.task_list[priority].append(task)
+            print('Удалили задачу {}'.format(self.task_list[priority].pop()))
+            if len(str(self.task_list[priority])) == 0:
+                self.task_list.pop(priority)
         else:
-            self.task_list[priority] = [task]
+            print('Задача с данным приоритетом отсутствует!')
 
     def __str__(self):
         """
         Магический метод вывода задач по приоритету: чем меньше число, тем выше задача
         key (int): ключ
-        value (str): значение
         result (str): результат для вывода на экран
         """
-        sort = sorted(self.task_list.items())
         result = ''
-        for key, value in sort:
-            result += '{}: '.format(key) + '; '.join(value) + '\n'
+        for key in sorted(self.task_list.keys()):
+            result += str(key) + ' ' + str(self.task_list[key]) + '\n'
         return 'Результат:\n{}'.format(result)
 
-
-"""
-У вас в словарь попадает название задачи, а должен попадать экземпляр класса Stack(), 
-т.к. в задании " Сам менеджер работает на основе Стэка (не наследование!). 
-Иначе для чего у вас класс Stek ?? 
-
-Подскажу архитектуру. TaskManager содержит словарь {приоритет, Стек}. 
-При добавлении добавляется задача в стек(если уже есть задачи с таким приоритетом) 
-или добавляется Стек и одна задача в него.
-"""
 
 manager = TaskManager()
 manager.new_task("сделать уборку", 4)
@@ -66,4 +83,6 @@ manager.new_task("помыть посуду", 4)
 manager.new_task("отдохнуть", 1)
 manager.new_task("поесть", 2)
 manager.new_task("сдать дз", 2)
+print(manager)
+manager.delete_task(2)
 print(manager)
