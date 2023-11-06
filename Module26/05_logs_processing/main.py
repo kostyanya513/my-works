0b400070afbdb6bf77c2aa7cde67f7477bfd0b4d
Kot <kostyan513@mail.ru>
2023-11-06 16:06:03 +0300

    done 26.5: logs processing

--- Module26/05_logs_processing/main.py
+++ Module26/05_logs_processing/main.py
@@ -1,16 +1,27 @@
-# TODO здесь писать код
+import os
 
 
-# При помощи модуля os (и функции join) сформируйте пути до файлов work_logs.txt и output.txt в папке data
-# (output.txt может не быть в папке data, но его нужно будет там создать, при помощи кода)
-input_file_path = ...
-output_file_path = ...
-# Документация по join https://docs-python.ru/standart-library/modul-os-path-python/funktsija-join-modulja-os-path/
+def checking_file(file):
+    if os.path.exists(file):
+        print('Файл {} существует'.format(file))
 
-# Не забудьте проверить наличие файлов перед тем как начать работу с ними
-# https://docs-python.ru/standart-library/modul-os-path-python/funktsija-exists-modulja-os-path/
 
-with open(output_file_path, 'w') as output:
+def error_log_generator(path):
+    with open(path, 'r', encoding='utf8') as file:
+        for elem in file.read().split('\n'):
+            if not elem.find('ERROR'):
+                yield elem
+
+
+input_file_path = os.path.abspath(os.path.join('data', 'work_logs.txt'))
+output_file_path = os.path.abspath(os.path.join('data', 'output.txt'))
+
+checking_file(input_file_path)
+checking_file(output_file_path)
+
+
+with open(output_file_path, 'w', encoding='utf8') as output:
     for error_line in error_log_generator(input_file_path):
-        output.write(error_line)
+        print(error_line)
+        output.write(error_line + '\n')
 print("Файл успешно обработан.")
