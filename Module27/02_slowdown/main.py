6a49518c32e93459bd93b32ffda30aaad62127dd
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-11-18 21:56:00 +0300

    done 27.2: slowdown

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import time
+import functools
+from typing import Callable
+
+
+def delta_time(func: Callable) -> Callable:
+    """ Декоратор, который перед выполнением функции ждет несколько секунд """
+
+    @functools.wraps(func)
+    def wrapper():
+        time.sleep(3)
+        return func()
+
+    return wrapper
+
+
+@delta_time
+def calculate_cube() -> str:
+    """
+    Функция, которая выводит на экран текст
+    :return: строка
+    """
+    return 'Hello!'
+
+
+print('Результат:\t{}'.format(calculate_cube()))
+print('\nФункция:\t{};\nДокументация:\t{}'.format(
+    calculate_cube.__name__,
+    calculate_cube.__doc__
+))
