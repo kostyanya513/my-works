79b7ab7ce29fccd0299e685e99ed9e18f2436f46
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-11-18 21:53:32 +0300

    done 27.1: how are you

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+
+def how_are_you(func: Callable) -> Callable:
+    """
+    Декоратор, который спрашивает КАК ДЕДА?
+    Затем при любом ответе запускает дерируемую функцию.
+    """
+    @functools.wraps(func)
+    def wrapper():
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        return print(func())
+
+    return wrapper
+
+
+@how_are_you
+def test() -> str:
+    """
+    Функция, которая выводит на экран строку
+    :return: строка
+    """
+    return '<Тут что-то происходит...>'
+
+
+test()
+print('\nФункция:\t{};\nДокументация:\t{}'.format(test.__name__, test.__doc__))
