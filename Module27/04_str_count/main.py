1c8c08c612b0135cc74dab37703e951595ebad95
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-11-18 21:56:45 +0300

    done 27.4: str count

--- Module27/04_str_count/main.py
+++ Module27/04_str_count/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+
+
+def counter(func: Callable) -> Callable:
+    """
+    Декоратор, который считывает и выводит количество вызовов
+    декорируемой функции
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        return func(*args, **kwargs)
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def dec_func() -> None:
+    """
+    Функция, которая ничего не выполняет
+    """
+    pass
+
+
+enter_func = int(input('Сколько раз вызвать функцию? '))
+for _ in range(enter_func):
+    dec_func()
+print('Функция {} вызвана {} раз.'.format(dec_func.__name__, dec_func.count))
+print('Документация:\t{}'.format(dec_func.__doc__))
