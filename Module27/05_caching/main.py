4de6e3a7b52076a07920d5fa0b6e4ed8988219d2
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-11-20 22:19:55 +0300

    done 27.5: caching

--- Module27/05_caching/main.py
+++ Module27/05_caching/main.py
@@ -1,7 +1,44 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
 
 
-def fibonacci(number):
+def decorator(func: Callable) -> Callable:
+    """
+    Декоратор, который выполняет задвнную функцию
+    """
+
+    @functools.wraps(func)
+    def wrapper(num):
+        value = func(num)
+        return value
+
+    return wrapper
+
+
+def cache_args(func: Callable) -> Callable:
+    """
+    Декоратор, который создает словарь из аргументов и значений функции
+    и при повторном вызове с теми же аргументами возвращает сохраненный резкльтат
+    """
+    dt = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        if args not in dt:  # При каждом вызове проверяйте, не было ли уже аналогичного вызова
+            dt[args] = func(*args)
+        return dt[args]
+
+    return wrapper
+
+
+@cache_args
+@decorator
+def fibonacci(number: int) -> int:
+    """
+    Функция для вычисления чисел Фебоначчи
+    :param number: int
+    :return: int
+    """
     if number <= 1:
         return number
     return fibonacci(number - 1) + fibonacci(number - 2)
@@ -9,9 +46,9 @@ def fibonacci(number):
 
 # Вычисление числа Фибоначчи с использованием кеширования
 print(fibonacci(10))  # Результат будет кеширован
-
-# Повторное вычисление числа Фибоначчи с теми же аргументами
+#
+# # Повторное вычисление числа Фибоначчи с теми же аргументами
 print(fibonacci(10))  # Результат будет взят из кеша
-
-# Вычисление числа Фибоначчи с другим аргументом
+#
+# # Вычисление числа Фибоначчи с другим аргументом
 print(fibonacci(5))  # Результат будет вычислен и закеширован
