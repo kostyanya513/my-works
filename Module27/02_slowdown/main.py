c49323dab734385890e26b22cb39acd4f2214909
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-11-21 22:32:17 +0300

    fixe 27.2: showdown

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,29 +1,29 @@
 import time
 import functools
-from typing import Callable
+from typing import Callable, Any
 
 
 def delta_time(func: Callable) -> Callable:
     """ Декоратор, который перед выполнением функции ждет несколько секунд """
 
     @functools.wraps(func)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         time.sleep(3)
-        return func()
+        return func(*args, **kwargs)
 
     return wrapper
 
 
 @delta_time
-def calculate_cube() -> str:
+def calculate_cube() -> Any:
     """
     Функция, которая выводит на экран текст
     :return: строка
     """
-    return 'Hello!'
+    print('Hello!')
 
 
-print('Результат:\t{}'.format(calculate_cube()))
+calculate_cube()
 print('\nФункция:\t{};\nДокументация:\t{}'.format(
     calculate_cube.__name__,
     calculate_cube.__doc__
