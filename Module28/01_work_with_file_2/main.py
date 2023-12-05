aa7f8c4a1aec140e8cea63b0f8793c8c9ae90203
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-12-05 21:47:14 +0300

    done 28.1: work with file 2

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import os
+from typing import TextIO
+
+
+class File:
+    """
+    Класс создает и открывает в режиме записи несуществующий файл.
+    На выходе подавляются все исключения, связанные с файлами.
+    """
+    def __init__(self, efile) -> None:
+        self.efile = efile
+        self.i_file = None
+
+    def __enter__(self) -> TextIO:
+        if not os.path.exists(self.efile):
+            self.i_file = open(self.efile, 'w', encoding='utf8')
+            return self.i_file
+        self.i_file = open(self.efile, 'w', encoding='utf8')
+        print('Файл уже существует')
+        return self.i_file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.i_file.close()
+        if exc_type:
+            return True
+
+
+with File('example.txt') as file:
+    file.write('Действие')
