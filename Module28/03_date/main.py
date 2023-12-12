576d03cbc4332c21d63f6715047989a1c302a1cc
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-12-12 19:19:08 +0300

    fixe 28.3: date

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,43 +1,55 @@
 class Date:
     """
-    Класс проверяет дату на корректность
+    Класс проверяет дату на корректность и конвертирует строку даты в объект класса
     Args:
-        text_data (str): дата
+        day (int): число месяца
+        month (int): месяц года
+        year (int): год
     """
-    def __init__(self, text_data: str) -> None:
-        self.is_date_valid(text_data)
+    def __init__(self, day: int = 0, month: int = 0, year: int = 0) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self) -> str:
+        return 'День: {}\tМесяц: {}\tГод: {}'.format(
+            self.day, self.month, self.year
+        )
 
     @classmethod
-    def from_string(cls, text_data: str) -> str:
+    def from_string(cls, text_data: str) -> 'Date':
         """
-        Метод для конвертации даты
-        :param text_data: дата
-        :return: дата
+        Метод для конвертации строки даты в объект класса Date
+        :param text_data: строка даты
+        :return: объект класса Date
         """
-        return 'День: {}\tМесяц: {}\tГод: {}'.format(
-            text_data.split('-')[0],
-            text_data.split('-')[1],
-            text_data.split('-')[2]
-        )
+        int_data = text_data.split('-')
+        day, month, year = int(int_data[0]), int(int_data[1]), int(int_data[2])
+        data_object = cls(day, month, year)
+        if data_object.is_date_valid(text_data):
+            return data_object
+        else:
+            print('Некорректная дата!')
 
     @classmethod
     def is_date_valid(cls, text_data: str) -> bool:
         """
-        Метод для проверки даты на корректность
-        :param text_data: дата
+        Метод для проверки даты на корректность даты
+        :param text_data: строка даты
         :return: bool
         """
-        int_data = [int(item) for item in text_data.split('-')]
-        if 1 <= int_data[1] <= 12 or int_data[2] < 0:
-            if (int_data[1] == 2 and 1 <= int_data[0] <= 28) \
-                    or (int_data[1] in [1, 3, 5, 7, 8, 10, 12] and 1 <= int_data[0] <= 31) \
-                    or (int_data[1] in [4, 6, 9, 11] and 1 <= int_data[0] <= 30):
+        int_data = text_data.split('-')
+        day, month, year = int(int_data[0]), int(int_data[1]), int(int_data[2])
+        if 1 <= month <= 12 or year < 0:
+            if (int_data[1] == 2 and 1 <= day <= 28) \
+                    or (month in [1, 3, 5, 7, 8, 10, 12] and 1 <= day <= 31) \
+                    or (month in [4, 6, 9, 11] and 1 <= day <= 30):
                 return True
             else:
                 return False
 
 
-date = Date.from_string('10-12-2077')
+date = Date.from_string('90-12-2077')
 print(date)
 print(Date.is_date_valid('10-12-2077'))
 print(Date.is_date_valid('40-12-2077'))
