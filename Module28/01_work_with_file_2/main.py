0dd91ec0d349b03f034ab93a33ccfb666cc0eea5
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-12-12 18:11:19 +0300

    fixe 28.1: work with file 2

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,29 +1,31 @@
 import os
-from typing import TextIO
+from typing import TextIO, Optional
 
 
 class File:
     """
     Класс создает и открывает в режиме записи несуществующий файл.
     На выходе подавляются все исключения, связанные с файлами.
+    Args:
+        efile (str) имя файла
+        file_mode (str) режим открытия файла
     """
-    def __init__(self, efile) -> None:
+    def __init__(self, efile: str, file_mode: str) -> None:
         self.efile = efile
+        self.file_mode = file_mode
         self.i_file = None
 
     def __enter__(self) -> TextIO:
-        if not os.path.exists(self.efile):
-            self.i_file = open(self.efile, 'w', encoding='utf8')
-            return self.i_file
-        self.i_file = open(self.efile, 'w', encoding='utf8')
-        print('Файл уже существует')
+        if os.path.exists(self.efile):
+            print('Файл уже существует')
+        self.i_file = open(self.efile, self.file_mode, encoding='utf8')
         return self.i_file
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> Optional[bool]:
         self.i_file.close()
-        if exc_type:
+        if exc_type is IOError:
             return True
 
 
-with File('example.txt') as file:
+with File('example.txt', 'w') as file:
     file.write('Действие')
