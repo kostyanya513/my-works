7d8979e1234180cb3d458bbd6f6b81088b538628
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-12-20 15:57:10 +0300

    done 28.1: permissions

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+from collections.abc import Callable
+import functools
+
+user_permissions = ['admin']
+
+
+def check_permission(access: str) -> Callable:
+    """
+    Декоратор проверяет, есть ли у пользователя доступ
+    к вызываемой функции
+    """
+
+    def user_decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if access in user_permissions:
+                return func(*args, **kwargs)
+            else:
+                print('PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию {}'.format(
+                    func.__name__))
+
+        return wrapped
+
+    return user_decorator
+
+
+@check_permission('admin')
+def delete_site() -> None:
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment() -> None:
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
