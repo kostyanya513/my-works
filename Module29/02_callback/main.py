c245a37b41c6e90ff300f06688ef009e95e11c32
Konstantin Fedosenkov <kostyan513@mail.ru>
2023-12-23 17:21:25 +0300

    done 28.2: callback

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+from typing import Callable, Dict
+
+app: Dict[str, Callable] = {}
+
+
+def callback(key: str) -> Callable:
+    """
+    Декоратор, добавляет в словарь функцию
+    """
+    def decorator_func(func: Callable) -> Callable:
+        app[key] = func
+        return func
+    return decorator_func
+
+
+@callback('//')
+def example() -> str:
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+print(route)
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+print(app)
