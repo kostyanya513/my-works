fea0119c87b7142a64ea7dfa0832fe0e54079d59
Konstantin Fedosenkov <kostyan513@mail.ru>
2024-01-02 18:08:34 +0300

    done 29.5: singleton

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def singleton(cls) -> Callable:
+    """
+    Декоратор возвращает экземпляр класса
+    """
+    @functools.wraps(cls)
+    def wrapped(*args, **kwargs):
+        if not cls(*args, **kwargs):
+            return cls
+
+    return wrapped
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
