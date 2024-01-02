9aa0eca3c31bd5fe32b03baddffa4d549489be9e
Konstantin Fedosenkov <kostyan513@mail.ru>
2024-01-02 18:53:54 +0300

    done 29.6: decorator class

--- Module29/06_decorator_class/main.py
+++ Module29/06_decorator_class/main.py
@@ -1,8 +1,26 @@
-# TODO здесь писать код
+import time
+from typing import Callable
+import functools
+
+
+class LoggerDecorator:
+    def __init__(self, func: Callable) -> None:
+        functools.update_wrapper(self, func)
+        self.func = func
+
+    def __call__(self, *args, **kwargs) -> None:
+        print('Вызов функнции {}\nАргументы: {}'.format(self.func.__name__, args, kwargs))
+        start = time.time()
+        res = self.func(*args, **kwargs)
+        end = time.time()
+        execution_time = end - start
+        print('Результат: {res}\nВремя выполнения: {time} секунд'.format(
+            res=res,
+            time=execution_time))
 
 
 @LoggerDecorator
-def complex_algorithm(arg1, arg2):
+def complex_algorithm(arg1: int, arg2: int) -> int:
     # Здесь выполняется сложный алгоритм
     result = 0
     for i in range(arg1):
