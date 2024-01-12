403a489ab65682a3e749eaaedaf8e9852d59113d
Konstantin Fedosenkov <kostyan513@mail.ru>
2024-01-12 14:42:06 +0300

    done 30.3: palindrome the return

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(string: str) -> bool:
+    res = collections.deque(string)  # возвращаем последовательность
+    while len(res) > 1:  # цикл, проверяет уменьшение длины последовательности
+        if collections.deque.popleft(res) != collections.deque.pop(res):  # сравниваем первый и последний удаленные # элементы последовательности
+            return False
+    return True
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbba'))
