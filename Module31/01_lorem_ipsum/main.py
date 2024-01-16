bfba4a2050d007f8d808c4b81527b693b5097640
Konstantin Fedosenkov <kostyan513@mail.ru>
2024-01-16 14:40:37 +0300

    done 31.1: lorem ipsum

--- Module31/01_lorem_ipsum/main.py
+++ Module31/01_lorem_ipsum/main.py
@@ -1,7 +1,12 @@
+import re
+
 text = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
 nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
 Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate 
 """
 
-# TODO здесь писать код
+word_pattern = r'\b\w{4}\b'
+
+list_words = re.findall(word_pattern, text)
+print(list_words)
