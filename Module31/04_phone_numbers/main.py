2327eb092b9e9f4b01b52f90fbe41acb53bff668
Konstantin Fedosenkov <kostyan513@mail.ru>
2024-01-20 14:03:47 +0300

    done 31.4: phone numbers

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import re
+
+numbers = ['9999999999', '999999-999', '99999x9999']
+number_pattern = r'[8,9]\d{9}'
+
+
+for number in numbers:
+    res = re.search(number_pattern, number)
+    print(bool(res))
